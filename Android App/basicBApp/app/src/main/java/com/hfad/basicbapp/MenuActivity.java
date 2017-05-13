b56d662ac9c3c9e41c42456c6368fcb313e75128
Cornstarch Jones <coraeacoleman@gmail.com>
2017-05-13 01:40:59 -0400

    Adjusted display of username on Menu page
    Includes most recent Android files for Login, Signup, and Menu pages

--- Android App/basicBApp/app/src/main/java/com/hfad/basicbapp/MenuActivity.java
+++ Android App/basicBApp/app/src/main/java/com/hfad/basicbapp/MenuActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.Arrays;
+
 public class MenuActivity extends AppCompatActivity {
 
     @Override
@@ -14,11 +16,12 @@ public class MenuActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String message = intent.getStringExtra(LoginActivity.EXTRA_MESSAGE);
-
+        String[] split_message = message.split("[@._]");
+        String user_name = split_message[0];
         // Capture the layout's TextView and set the string as its text
         //Grabbing the Textview to display the username
         TextView display_user = (TextView) findViewById(R.id.username);
-        display_user.setText("Welcome "+message);
+        display_user.setText("Welcome "+ user_name.toString());
 
     }
 }
