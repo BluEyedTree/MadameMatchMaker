b84afeb04f2cacdb36f48556c622a90f9e823104
coderTurk <ozan.gokdemir15@ncf.edu>
2017-05-21 18:33:04 -0400

    Request match UI design and logic.

---
+++ Android App/basicBApp/app/src/main/java/com/hfad/basicbapp/ViewMatches.java
@@ -0,0 +1,69 @@
+package com.hfad.basicbapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewMatches extends AppCompatActivity {
+    ListView list;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_matches);
+        dealWithMatches();
+    }
+
+    private void dealWithMatches(){
+        String output = getMatches();
+        String[] matched = output.split("&");
+        List<String> finalList = new ArrayList<String>();
+        for(String single : matched){
+            String[] singleSplitted = single.split(";");
+
+            for(int i = 0; i<singleSplitted.length;i++){
+                if(i==0){
+                    finalList.add(singleSplitted[0]);
+                }
+                else {
+                    String NewString = "    "+singleSplitted[i];
+                    finalList.add(NewString);
+                }
+            }
+        }
+        String[] finalS = finalList.toArray(new String[finalList.size()]);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,finalS);
+        list = (ListView)findViewById(R.id.matchesList);
+        list.setAdapter(adapter);
+    }
+
+
+
+
+    public String getMatches(){
+        GetPage task = new GetPage();
+        try {
+            String email = User.getEmail();
+            String BuildString = "http://10.0.2.2:5000/getMatches/"+email;
+            Log.i("TAT", BuildString);
+            String matches = task.execute(new String[] {BuildString}).get();
+                Log.i("TAT", matches);
+                return matches;
+
+        }
+        catch(Exception e){
+            Log.i("TAT", "Failed");
+            //display.setText(e.toString());
+            return "Failed";
+
+        }
+
+
+    }
+
+}
